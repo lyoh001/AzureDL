377f47ab67915f05d7bf43ee7ce41a3edf07a313
lyoh001@hotmail.com <lyoh001@hotmail.com>
2022-04-08 23:39:12 +1000

    Commit

--- api/env/__init__.py
+++ api/env/__init__.py
@@ -1,3 +1,30 @@
+# import logging
+# import warnings
+# from pickle import load
+
+# import azure.functions as func
+# import pandas as pd
+# import tensorflow as tf
+# from tensorflow import keras
+
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# warnings.filterwarnings("ignore")
+
+
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info("*******Starting main function*******")
+#     logging.info(f"Request query: {req.get_json()}")
+
+#     preprocessor = load(open("dl_preprocessor.pkl", "rb"))
+#     model = keras.models.load_model("dl_model")
+
+#     logging.info("*******Finishing main function*******")
+
+#     return func.HttpResponse(
+#         status_code=200,
+#         body=f"{model.predict(preprocessor.transform(pd.DataFrame(req.get_json())))}",
+#     )
+
 import json
 import logging
 import os
@@ -7,6 +34,4 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
-    return func.HttpResponse(
-        body=json.dumps({"text": os.environ["CONNECTION_STRING"]})
-    )
+    return func.HttpResponse(body=json.dumps({"text": os.environ["CONNECTION_STRING"]}))
