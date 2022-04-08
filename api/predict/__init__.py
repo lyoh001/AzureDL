57e3940f29b3e69c34cc679e20e5383f71c142d2
lyoh001@hotmail.com <lyoh001@hotmail.com>
2022-04-09 07:46:39 +1000

    Commit

---
+++ api/predict/__init__.py
@@ -0,0 +1,19 @@
+import warnings
+from pickle import load
+
+import azure.functions as func
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+warnings.filterwarnings("ignore")
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    preprocessor = load(open("dl_model/dl_preprocessor.pkl", "rb"))
+    model = keras.models.load_model("dl_model")
+    return func.HttpResponse(
+        # body=f"{model.predict(preprocessor.transform(pd.DataFrame(req.get_json())))}",
+        body=f"{req.get_json()}",
+    )
