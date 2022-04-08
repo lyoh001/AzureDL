6c9591009cc8d99e22a803dd7fe503e7b2f1a668
lyoh001@hotmail.com <lyoh001@hotmail.com>
2022-04-09 08:32:48 +1000

    Commit

--- api/predict/__init__.py
+++ api/predict/__init__.py
@@ -7,13 +7,13 @@ import azure.functions as func
 # import tensorflow as tf
 # from tensorflow import keras
 
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 warnings.filterwarnings("ignore")
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    preprocessor = load(open("dl_preprocessor/dl_preprocessor.pkl", "rb"))
-    model = keras.models.load_model("dl_model")
+    # preprocessor = load(open("dl_preprocessor/dl_preprocessor.pkl", "rb"))
+    # model = keras.models.load_model("dl_model")
     return func.HttpResponse(
         # body=f"{model.predict(preprocessor.transform(pd.DataFrame(req.get_json())))}",
         body=f"{req.get_json()}",
